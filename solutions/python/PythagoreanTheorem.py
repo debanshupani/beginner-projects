3ae8860e945307b20504b23a520ad5613a27465d
aekanshd <aekansh@gmail.com>
2018-01-08 23:19:28 +0530

    Fixed PythogreanTheroem.py

--- solutions/python/PythagoreanTheorem.py
+++ solutions/python/PythagoreanTheorem.py
@@ -1,6 +1,8 @@
 def checkPythogoreanTheorem(a,b,c):
     if(((a**2)==((b**2)+(c**2))) or ((b**2)==((c**2)+(a**2))) or ((c**2)==((a**2)+(b**2)))):
-        return True
+        return "Ohh yeah!"
+    else:
+        return "Nope."
 counter = "y"
 while(True):
     if(counter == "n" or counter == "N"):
