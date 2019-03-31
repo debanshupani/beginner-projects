2ea96fafea7ea7f946a3ff46a9d16df2e2d00b6e
debanshupani <31133634+debanshupani@users.noreply.github.com>
2019-03-31 11:17:03 +0530

    Update menuCalculator.py

--- solutions/python/menuCalculator.py
+++ solutions/python/menuCalculator.py
@@ -7,12 +7,11 @@ def addItem(price,name):
 def displayMenu():
     global items
     print("Here's the menu for today:\n")
-    print("ITEM_CODE\tITEM_NAME\tITEM_PRICE")
+    print("ITEM_CODE\tITEM_NAME\t\t\tITEM_PRICE")
     for n in range(0,len(items)):
-        print(str(n+1)+".\t\t\t"+str(items[n][0])+"\t\tRs."+str(items[n][1]))
+        print(str(n+1)+".\t\t\t"+str(items[n][0])+"\t\t₹"+str(items[n][1]))
 
 def calculatePrice(code):
-    #TODO: Add flags for items that are not present in the menu (like 0067)
     global items
     ordered_items=[]
     for item in code:
@@ -20,7 +19,18 @@ def calculatePrice(code):
 
     sum=0
     for item in ordered_items:
-        sum+=items[item-1][1]
+        try:
+            sum+=items[item-1][1]
+        except IndexError:
+            while(True):
+                print("The item numbers you have entered are not available")
+                print("Please enter valid item numbers")
+                choice = input("Do you want to order again? (Y/N)")
+                if (choice == "N" or choice == "n"):
+                    exit()
+                else:
+                    finalOrderMenu()
+
 
     return sum
 
@@ -46,28 +56,32 @@ def displayOrder(code):
         if (quantity > 0):
             processed_items = [y for y in processed_items if y != i + 1]
             print(str(i + 1) + ".\t" + str(items[i][1]) + "\t" + str(items[i][0]) + "\t" + str(
-                quantity) + "\tRs." + str(price))
+                quantity) + "\t₹" + str(price))
 
 
-addItem(10,"Samosa")
-addItem(15,"Aloo Tikki")
-addItem(35,"Bun Samosa")
-addItem(30,"Oreo Milkshake")
+addItem(10,"Samosa         ")
+addItem(15,"Aloo Tikki     ")
+addItem(35,"Bun Samosa     ")
+addItem(30,"Oreo Milkshake ")
 addItem(15,"Fresh Lime Soda")
 
-while(True):
-    print("Welcome to our store.\n")
-    displayMenu()
-    order=input("What would you like to order today? ")
-    displayOrder(order)
-    print()
-    print("Your  final bill amount is Rs."+str(calculatePrice(order)))
-    print()
-    choice=input("Do you want to order again? (Y/N)")
-    if(choice=="N" or choice=="n"):
+def finalOrderMenu():
+    while(True):
+        print("Welcome to our store.\n")
+        displayMenu()
+        order=input("What would you like to order today? ")
+        calculatePrice(order)
+        displayOrder(order)
+        print()
+        print("Your  final bill amount is Rs."+str(calculatePrice(order)))
+        print()
+        choice=input("Do you want to order again? (Y/N)")
+        if(choice=="N" or choice=="n"):
+            print()
+            print("==========================")
+            print("Thank you for shopping with us.")
+            break
         print()
         print("==========================")
-        print("Thank you for shopping with us.")
-        break
-    print()
-    print("==========================")
+
+finalOrderMenu()
